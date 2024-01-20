b971002642344bd23da43a907ee3345ed9082c8a
Jack Parsons <jparsons@ualberta.ca>
2024-01-20 18:24:36 +0000

    fix dockerfile

--- docker/setup.bash
+++ docker/setup.bash
@@ -1,4 +1,5 @@
-echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
-echo "source /f1tenth/dev_ws/install/local_setup.bash" >> ~/.bashrc
+echo -e "\nsource /opt/ros/humble/setup.bash" >> ~/.bashrc
+echo -e "\nsource /f1tenth/scripts/formula_bashrc.sh" >> ~/.bashrc
+# echo "source /f1tenth/dev_ws/install/local_setup.bash" >> ~/.bashrc
 source ~/.bashrc
 pre-commit install
