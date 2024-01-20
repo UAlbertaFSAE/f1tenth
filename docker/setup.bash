61d6982d69b8dcd2b53d527e8c0ef8e6fc6e4a30
Jack Parsons <jparsons@ualberta.ca>
2024-01-20 11:40:12 -0700

    simplify setup bash script

--- docker/setup.bash
+++ docker/setup.bash
@@ -1,5 +1,4 @@
-echo -e "\nsource /opt/ros/humble/setup.bash" >> ~/.bashrc
-echo -e "\nsource /f1tenth/scripts/formula_bashrc.sh" >> ~/.bashrc
-# echo "source /f1tenth/dev_ws/install/local_setup.bash" >> ~/.bashrc
+#!/usr/bin/env bash
+
+echo -e "\nsource /f1tenth/scripts/formula_bashrc.sh" >>~/.bashrc
 source ~/.bashrc
-pre-commit install
--- scripts/formula_bashrc.sh
+++ scripts/formula_bashrc.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+echo -e "\nsource /opt/ros/humble/setup.bash" >>~/.bashrc
+# echo "source /f1tenth/dev_ws/install/local_setup.bash" >> ~/.bashrc
+
 export FORMULA_HOME=$(
 	cd "$(dirname "${BASH_SOURCE[0]}")/.."
 	pwd -P
 )
+alias rc_home="cd $FORMULA_HOME"
 
 # unlimited bash history size
 export HISTFILESIZE=
@@ -12,3 +16,5 @@ export HISTSIZE=
 rc_clean() {
 	cd $FORMULA_HOME/dev_ws && rm -rf build install log
 }
+
+pre-commit install
