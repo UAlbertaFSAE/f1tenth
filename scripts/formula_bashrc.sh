45e072e32095e24a37cbc36e796ba34b38ec580d
Jack Parsons <jparsons@ualberta.ca>
2024-01-20 12:02:52 -0700

    add source and build commands

--- scripts/formula_bashrc.sh
+++ scripts/formula_bashrc.sh
@@ -17,4 +17,23 @@ rc_clean() {
 	cd $FORMULA_HOME/dev_ws && rm -rf build install log
 }
 
+rc_source() {
+	source $FORMULA_HOME/dev_ws/install/setup.bash
+}
+
+export CMAKE_BUILD_TYPE=RelWithDebInfo
+rc_build() {
+	local args=(
+		--executor parallel
+		--parallel-workers $(python3 -c "import math; print(math.ceil($(nproc) ** 0.5) + 1)")
+		--continue-on-error
+		--symlink-install
+		--cmake-args "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On"
+		-Wall -Wextra -Wpedantic
+	)
+
+	cd $FORMULA_HOME/dev_ws && colcon build "${args[@]}" "$@"
+}
+alias rc_all='rc_clean && rc_build && rc_source'
+
 pre-commit install
