a125e0a4e148d874213293295f01e2b8e0fbf94d
joey <jaq@ualberta.ca>
2024-03-23 14:11:11 -0600

    launch file for safety node

--- dev_ws/src/operations/safety_node/launch/safety_node.launch.py
+++ dev_ws/src/operations/safety_node/launch/safety_node.launch.py
@@ -1,2 +1,26 @@
-import launch
-import launch_ros.actions
+from launch_ros.actions import Node
+
+from launch import LaunchDescription
+
+
+# TODO: get range_min and max from zed2i yaml in perception zed_wrapper config folder
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Node(
+                package="depthimage_to_laserscan",
+                executable="depthimage_to_laserscan_node",
+                name="depthimage_to_laserscan",
+                remappings=[
+                    ("/depth", "/zed/zed_node/depth/depth_registered"),
+                    ("/depth_camera_info", "zed/zed_node/depth/camera_info"),
+                ],
+                parameters=[{"range_min": 0.2}, {"range_max": 20.0}, {"scan_height": 3}],
+            ),
+            Node(
+                package="safety_node",
+                executable="safety_node",
+                name="safety_node",
+            ),
+        ]
+    )
