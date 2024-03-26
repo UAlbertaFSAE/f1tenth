a33efe852b963ea1f93abe8cc6128242d667d1da
Jack Parsons <jparsons@ualberta.ca>
2024-03-25 20:35:48 -0600

    read csv file

--- dev_ws/src/navigation/detection_generator/include/detection_generator/detection_generator.hpp
+++ dev_ws/src/navigation/detection_generator/include/detection_generator/detection_generator.hpp
@@ -1,10 +1,13 @@
-#include <cstdio>
+#include <fstream>
+#include <string>
+#include <vector>
 #include "rc_interfaces/msg/cone.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 class DetectionGenerator : public rclcpp::Node {
  public:
   DetectionGenerator();
+  void read_csv(std::string path);
 
  private:
   std::vector<rc_interfaces::msg::Cone> cones;
--- dev_ws/src/navigation/detection_generator/src/detection_generator.cpp
+++ dev_ws/src/navigation/detection_generator/src/detection_generator.cpp
@@ -1,11 +1,51 @@
-#include "detection_generator/detection_generator.hpp"
+#include "../include/detection_generator/detection_generator.hpp"
 
 DetectionGenerator::DetectionGenerator() : Node("detection_generator_node") {}
+
+void DetectionGenerator::read_csv(std::string path) {
+  std::fstream file;
+  file.open(path);
+  RCLCPP_INFO(this->get_logger(), "%s", (file.is_open() ? "Opened file" : "Failed to open file"));
+
+  std::string line, word;
+  while (!file.eof()) {
+    std::getline(file, line, '\n');
+    std::stringstream s(line);
+
+    int j = 0;
+    rc_interfaces::msg::Cone cone;
+    while (getline(s, word, ',')) {
+      if (!word.empty()) {
+        if (j == 0) {
+          cone.x = std::stod(word);
+          RCLCPP_INFO(this->get_logger(), "X: %f", cone.x);
+        }
+
+        if (j == 1) {
+          cone.y = std::stod(word);
+          RCLCPP_INFO(this->get_logger(), "Y: %f", cone.y);
+        }
+
+        if (j == 2) {
+          cone.color = word;
+          RCLCPP_INFO(this->get_logger(), "Color: %s", cone.color.c_str());
+        }
+      }
+
+      j++;
+    }
+    cones.push_back(cone);
+  }
+
+  file.close();
+}
 
 int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
 
   auto node = std::make_shared<DetectionGenerator>();
+  node->read_csv("/f1tenth/dev_ws/src/navigation/detection_generator/data/cone_positions.csv");
+
   rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
