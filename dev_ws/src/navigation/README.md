cf8e35e7151539b7a8b3d0115b22298ae5888128
Jack Parsons <jparsons@ualberta.ca>
2024-03-30 11:08:59 -0600

    Cone Generator Node (#45)
    * create cone message
    
    * 2 new files created, map_corner_positions.csv leave this one for now, its just the corners of the rectangles for the map we are using, im just keeping it so we can reference it. cone_positions.csv is what will be the csv file that we read from for the entire maps cones, labelled x,y,color. If you want to visualize the cones, add the MarkerArray with /waypoints as the topic in rviz and then take our the colorout and place it in the waypoints_odom csv
    
    * Setup detection generator node that will take the .csv file of all the cones on the map, subscribe to the current position of the vehicle, and locate which nodes are in the vicinity specified by some radius. Created source code file, header file, and the cmake/package.xml so that building the node is succesful. Make sure to add any new dependencies to the cmake and package.xml as we progress on the development of the node
    
    * add cone message dependency
    
    * read csv file
    
    * move to perception
    
    * update readme
    
    * improve code
    
    * add math to calculate proper cones to publish
    
    * fix radius
    
    * comments
    
    * add odom subcription
    
    * use odom information to make it work
    
    * add cones message
    
    * fix quaternion
    
    * short circuit
    
    * fix csv
    
    * remove license
    
    ---------
    
    Co-authored-by: rydercatonio <rcatonio@ualberta.ca>

--- dev_ws/src/navigation/README.md
+++ dev_ws/src/navigation/README.md
@@ -1,4 +1,11 @@
 # Navigation
 
-### What goes here?
+## What goes here?
 The `navigation/` folder should contain any ROS packages that are related to motion planning and navigation
+
+## Packages
+### Pure Pursuit
+- An algorithm for the car to follow a set of waypoints
+
+### Waypoint Generator
+- A node to generate waypoints for pure pursuit
---
+++ dev_ws/src/perception/detection_generator/src/detection_generator.cpp
@@ -0,0 +1,107 @@
+#include "detection_generator/detection_generator.hpp"
+
+DetectionGenerator::DetectionGenerator() : Node("detection_generator_node") {
+  std::string csv_path =
+      "/f1tenth/dev_ws/src/perception/detection_generator/data/cone_positions.csv";
+  this->declare_parameter("radius", 5.0);
+  this->declare_parameter("odom_topic", "/ego_racecar/odom");
+
+  radius = this->get_parameter("radius").as_double();
+  std::string odom_topic = this->get_parameter("odom_topic").as_string();
+
+  cones = read_csv(csv_path);
+  cone_publisher = create_publisher<rc_interfaces::msg::Cones>("cone_data", rclcpp::QoS(10));
+  odom_subscriber = this->create_subscription<nav_msgs::msg::Odometry>(
+      odom_topic, rclcpp::QoS(10), std::bind(&DetectionGenerator::publish_cones, this, _1));
+}
+
+std::vector<rc_interfaces::msg::Cone> DetectionGenerator::read_csv(std::string path) {
+  std::fstream file;
+  file.open(path);
+  RCLCPP_INFO(this->get_logger(), "%s", (file.is_open() ? "Opened file" : "Failed to open file"));
+
+  std::string line, word;
+  std::vector<rc_interfaces::msg::Cone> cones;
+  while (!file.eof()) {
+    std::getline(file, line, '\n');
+    std::stringstream s(line);
+
+    int j = 0;
+    rc_interfaces::msg::Cone cone;
+    while (getline(s, word, ',')) {
+      if (!word.empty()) {
+        if (j == 0) {
+          cone.x = std::stod(word);
+          RCLCPP_INFO(this->get_logger(), "X: %f", cone.x);
+        }
+
+        if (j == 1) {
+          cone.y = std::stod(word);
+          RCLCPP_INFO(this->get_logger(), "Y: %f", cone.y);
+        }
+
+        if (j == 2) {
+          cone.color = word;
+          RCLCPP_INFO(this->get_logger(), "Color: %s", cone.color.c_str());
+        }
+      }
+
+      j++;
+    }
+    cones.push_back(cone);
+  }
+
+  file.close();
+  return cones;
+}
+
+double distance(float x1, float y1, float x2, float y2) {
+  return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+}
+
+void DetectionGenerator::publish_cones(const nav_msgs::msg::Odometry::ConstSharedPtr odom) {
+  if (cones.size() == 0) {
+    RCLCPP_INFO(this->get_logger(), "No cones to process");
+    return;
+  }
+
+  float carX = odom->pose.pose.position.x;
+  float carY = odom->pose.pose.position.y;
+  Eigen::Matrix3d rotation_matrix =
+      Eigen::Quaterniond(odom->pose.pose.orientation.w, odom->pose.pose.orientation.x,
+                         odom->pose.pose.orientation.y, odom->pose.pose.orientation.z)
+          .toRotationMatrix();
+
+  rc_interfaces::msg::Cones visible_cones;
+  for (const auto& cone : cones) {
+    double dist = distance(carX, carY, cone.x, cone.y);
+    if (dist > radius) {
+      continue;
+    }
+
+    // calculate vector from current position to cone
+    Eigen::Vector3d cone_vector(cone.x - carX, cone.y - carY, 0.0);
+
+    // transform vector to car's coordinate frame using rotation matrix
+    Eigen::Vector3d transformed_vector = rotation_matrix.transpose() * cone_vector;
+
+    // check if the cone is within the radius and in front of the current position
+    if (dist <= radius && transformed_vector.x() >= 0) {
+      RCLCPP_INFO(this->get_logger(), "Found cone: x=%f, y=%f, color=%s", cone.x, cone.y,
+                  cone.color.c_str());
+      visible_cones.cones.push_back(cone);
+    }
+  }
+
+  RCLCPP_INFO(this->get_logger(), "Publishing %ld visible cones", visible_cones.cones.size());
+  cone_publisher->publish(visible_cones);
+}
+
+int main(int argc, char** argv) {
+  rclcpp::init(argc, argv);
+
+  auto node = std::make_shared<DetectionGenerator>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
