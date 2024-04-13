9a09d2471274efc1e4759ea4e25ae00510e55dd3
Jack Parsons <jparsons@ualberta.ca>
2024-04-13 17:37:53 +0000

    small changes

--- dev_ws/src/navigation/waypoint_triangulation/src/triangulator.cpp
+++ dev_ws/src/navigation/waypoint_triangulation/src/triangulator.cpp
@@ -2,13 +2,17 @@
 
 Triangulator::Triangulator() : Node("triangulator") {
   this->declare_parameter("cones_topic", "/cone_data");
-  std::string cone_topic = this->get_parameter("cone_topic").as_string();
+  std::string cone_topic = this->get_parameter("cones_topic").as_string();
 
   cone_subscriber = this->create_subscription<rc_interfaces::msg::Cones>(
       cone_topic, rclcpp::QoS(10), std::bind(&Triangulator::read_cones, this, _1));
 }
 
-void Triangulator::read_cones(const rc_interfaces::msg::Cones::ConstSharedPtr cones) {}
+void Triangulator::read_cones(const rc_interfaces::msg::Cones::ConstSharedPtr cones) {
+  for (int i = 0; i < cones->cones.size(); i++) {
+    std::cout << "X: " << cones->cones[i].x << " Y: " << cones->cones[i].y << std::endl;
+  }
+}
 
 int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
