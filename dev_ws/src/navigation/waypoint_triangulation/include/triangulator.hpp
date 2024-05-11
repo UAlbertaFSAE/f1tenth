b414f9bae9fdbb16bdba6c12c7418880932901a0
Jack Parsons <jparsons@ualberta.ca>
2024-05-11 19:59:40 +0000

    add odom callback splitting left and right cones

--- dev_ws/src/navigation/waypoint_triangulation/include/triangulator.hpp
+++ dev_ws/src/navigation/waypoint_triangulation/include/triangulator.hpp
@@ -1,8 +1,17 @@
+#include "nav_msgs/msg/odometry.hpp"
 #include "rc_interfaces/msg/cone.hpp"
 #include "rc_interfaces/msg/cones.hpp"
 #include "rclcpp/rclcpp.hpp"
 
+#include <limits.h>
+
 using std::placeholders::_1;
+
+// Left-Right pair of cone vectors
+struct LR {
+  std::vector<rc_interfaces::msg::Cone> left;
+  std::vector<rc_interfaces::msg::Cone> right;
+};
 
 /**
  * Maintain two arrays, left and right of the same size which are the positions of the left and
@@ -15,13 +24,22 @@ class Triangulator : public rclcpp::Node {
   ~Triangulator();
 
  private:
+  const std::string LEFT = "blue";
+  const std::string RIGHT = "yellow";
+
   rc_interfaces::msg::Cones* last_cones;
   rclcpp::Subscription<rc_interfaces::msg::Cones>::SharedPtr cone_subscriber;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscriber;
 
-  bool is_same_cone(const rc_interfaces::msg::Cone& coneA, const rc_interfaces::msg::Cone& coneB);
+  // call backs
   void read_cones(const rc_interfaces::msg::Cones::ConstSharedPtr cones);
+  void odom_callback(const nav_msgs::msg::Odometry::ConstSharedPtr odom);
+
+  double distance(float x1, float y1, float x2, float y2);
+  bool is_same_cone(const rc_interfaces::msg::Cone& coneA, const rc_interfaces::msg::Cone& coneB);
   bool has_new_cones(const rc_interfaces::msg::Cones::ConstSharedPtr cones);
-  //   rc_interfaces::msg::Cone closest_cone(const rc_interfaces::msg::Cones::ConstSharedPtr cones);
+  LR split(rc_interfaces::msg::Cones* cones);
+  rc_interfaces::msg::Cone closest_cone(std::vector<rc_interfaces::msg::Cone>& cones,
+                                        const geometry_msgs::msg::Point& position);
   //   rc_interfaces::msg::Cone next_cone();
-  //   bool has_new_cones(const rc_interfaces::msg::Cones::ConstSharedPtr cones);
 };
--- dev_ws/src/navigation/waypoint_triangulation/src/triangulator.cpp
+++ dev_ws/src/navigation/waypoint_triangulation/src/triangulator.cpp
@@ -2,10 +2,15 @@
 
 Triangulator::Triangulator() : Node("triangulator_node") {
   this->declare_parameter("cones_topic", "/detection_generator/cone_data");
+  this->declare_parameter("odom_topic", "/ego_racecar/odom");
   std::string cone_topic = this->get_parameter("cones_topic").as_string();
+  std::string odom_topic = this->get_parameter("odom_topic").as_string();
 
   cone_subscriber = this->create_subscription<rc_interfaces::msg::Cones>(
       cone_topic, rclcpp::QoS(10), std::bind(&Triangulator::read_cones, this, _1));
+
+  odom_subscriber = this->create_subscription<nav_msgs::msg::Odometry>(
+      odom_topic, rclcpp::QoS(10), std::bind(&Triangulator::odom_callback, this, _1));
 
   last_cones = new rc_interfaces::msg::Cones();
 
@@ -16,7 +21,8 @@ Triangulator::~Triangulator() { delete last_cones; }
 
 void Triangulator::read_cones(const rc_interfaces::msg::Cones::ConstSharedPtr cones) {
   if (!has_new_cones(cones)) {
-    RCLCPP_INFO(this->get_logger(), "No new cones to process");
+    RCLCPP_INFO(this->get_logger(), "No new cones to process, still has %ld cones",
+                cones->cones.size());
     return;
   }
 
@@ -27,6 +33,29 @@ void Triangulator::read_cones(const rc_interfaces::msg::Cones::ConstSharedPtr co
     RCLCPP_INFO(this->get_logger(), "Triangulator got cone: X: %f, Y: %f, Color: %s", cone.x,
                 cone.y, cone.color.c_str());
   }
+}
+
+void Triangulator::odom_callback(const nav_msgs::msg::Odometry::ConstSharedPtr odom) {
+  if (last_cones->cones.size() == 0) {
+    RCLCPP_INFO(this->get_logger(), "No cones for odom to process");
+    return;
+  }
+
+  // split into left and right
+  LR pair = split(last_cones);
+
+  // find closest left and closest right
+  rc_interfaces::msg::Cone left = closest_cone(pair.left, odom->pose.pose.position);
+  rc_interfaces::msg::Cone right = closest_cone(pair.right, odom->pose.pose.position);
+
+  RCLCPP_INFO(this->get_logger(), "Closest left cone: X: %f, Y: %f, Color: %s", left.x, left.y,
+              left.color.c_str());
+  RCLCPP_INFO(this->get_logger(), "Closest right cone: X: %f, Y: %f, Color: %s", right.x, right.y,
+              right.color.c_str());
+
+  // publish waypoint in mid point
+
+  // continue finding next closest left and rights based on previous angle
 }
 
 bool Triangulator::is_same_cone(const rc_interfaces::msg::Cone &coneA,
@@ -50,6 +79,42 @@ bool Triangulator::has_new_cones(const rc_interfaces::msg::Cones::ConstSharedPtr
   }
 
   return false;
+}
+
+LR Triangulator::split(rc_interfaces::msg::Cones *cones) {
+  LR pair;
+
+  for (size_t i = 0; i < cones->cones.size(); i++) {
+    rc_interfaces::msg::Cone cone = cones->cones[i];
+    if (cone.color == LEFT) {
+      pair.left.push_back(cone);
+    } else if (cone.color == RIGHT) {
+      pair.right.push_back(cone);
+    } else {
+      RCLCPP_INFO(this->get_logger(), "Found cone without left or right color");
+    }
+  }
+
+  return pair;
+}
+
+rc_interfaces::msg::Cone Triangulator::closest_cone(std::vector<rc_interfaces::msg::Cone> &cones,
+                                                    const geometry_msgs::msg::Point &position) {
+  double dist = std::numeric_limits<double>::max();
+  rc_interfaces::msg::Cone closest;
+
+  // find the closest cone
+  for (const auto &cone : cones) {
+    if (distance(position.x, position.y, cone.x, cone.y) < dist) {
+      closest = cone;
+    }
+  }
+
+  return closest;
+}
+
+double Triangulator::distance(float x1, float y1, float x2, float y2) {
+  return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 }
 
 int main(int argc, char **argv) {
