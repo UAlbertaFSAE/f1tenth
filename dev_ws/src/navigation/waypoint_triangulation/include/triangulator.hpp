06704a49f060ca4518f2ce71dbf61e1fee326f06
Jack Parsons <jparsons@ualberta.ca>
2024-05-11 17:21:36 +0000

    fix has new cones

--- dev_ws/src/navigation/waypoint_triangulation/include/triangulator.hpp
+++ dev_ws/src/navigation/waypoint_triangulation/include/triangulator.hpp
@@ -4,16 +4,21 @@
 
 using std::placeholders::_1;
 
+/**
+ * Maintain two arrays, left and right of the same size which are the positions of the left and
+ * right cones Use these two arrays to interpolate points between theme and publish these points for
+ * pure pursuit
+ */
 class Triangulator : public rclcpp::Node {
  public:
   Triangulator();
   ~Triangulator();
 
  private:
-  const rc_interfaces::msg::Cones* last_cones;
+  rc_interfaces::msg::Cones* last_cones;
   rclcpp::Subscription<rc_interfaces::msg::Cones>::SharedPtr cone_subscriber;
 
-  bool is_same_cone(const rc_interfaces::msg::Cone coneA, const rc_interfaces::msg::Cone coneB);
+  bool is_same_cone(const rc_interfaces::msg::Cone& coneA, const rc_interfaces::msg::Cone& coneB);
   void read_cones(const rc_interfaces::msg::Cones::ConstSharedPtr cones);
   bool has_new_cones(const rc_interfaces::msg::Cones::ConstSharedPtr cones);
   //   rc_interfaces::msg::Cone closest_cone(const rc_interfaces::msg::Cones::ConstSharedPtr cones);
--- dev_ws/src/navigation/waypoint_triangulation/src/triangulator.cpp
+++ dev_ws/src/navigation/waypoint_triangulation/src/triangulator.cpp
@@ -1,13 +1,15 @@
 #include "triangulator.hpp"
 
 Triangulator::Triangulator() : Node("triangulator_node") {
-  this->declare_parameter("cones_topic", "/cone_data");
+  this->declare_parameter("cones_topic", "/detection_generator/cone_data");
   std::string cone_topic = this->get_parameter("cones_topic").as_string();
 
   cone_subscriber = this->create_subscription<rc_interfaces::msg::Cones>(
       cone_topic, rclcpp::QoS(10), std::bind(&Triangulator::read_cones, this, _1));
 
   last_cones = new rc_interfaces::msg::Cones();
+
+  RCLCPP_INFO(this->get_logger(), "Starting up triangulator");
 }
 
 Triangulator::~Triangulator() { delete last_cones; }
@@ -18,25 +20,31 @@ void Triangulator::read_cones(const rc_interfaces::msg::Cones::ConstSharedPtr co
     return;
   }
 
+  last_cones->cones.clear();
   for (size_t i = 0; i < cones->cones.size(); i++) {
     rc_interfaces::msg::Cone cone = cones->cones[i];
+    last_cones->cones.push_back(cone);
     RCLCPP_INFO(this->get_logger(), "Triangulator got cone: X: %f, Y: %f, Color: %s", cone.x,
                 cone.y, cone.color.c_str());
   }
 }
 
-bool Triangulator::is_same_cone(const rc_interfaces::msg::Cone coneA,
-                                const rc_interfaces::msg::Cone coneB) {
+bool Triangulator::is_same_cone(const rc_interfaces::msg::Cone &coneA,
+                                const rc_interfaces::msg::Cone &coneB) {
   return (coneA.x == coneB.x) && (coneA.y == coneB.y) && (coneA.color == coneB.color);
 }
 
 bool Triangulator::has_new_cones(const rc_interfaces::msg::Cones::ConstSharedPtr cones) {
-  if (cones->cones.size() != last_cones->cones.size()) {
+  if (last_cones->cones.size() == 0) {
     return true;
   }
 
-  for (size_t i = 0; i < cones->cones.size(); i++) {
+  for (size_t i = 0; i < last_cones->cones.size(); i++) {
     if (!is_same_cone(cones->cones[i], last_cones->cones[i])) {
+      RCLCPP_INFO(
+          this->get_logger(), "Different Cones: last=(x=%f, y=%f, c=%s). new=(x=%f, y=%f, c=%s)",
+          last_cones->cones[i].x, last_cones->cones[i].y, last_cones->cones[i].color.c_str(),
+          cones->cones[i].x, cones->cones[i].y, cones->cones[i].color.c_str());
       return true;
     }
   }
@@ -44,7 +52,7 @@ bool Triangulator::has_new_cones(const rc_interfaces::msg::Cones::ConstSharedPtr
   return false;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
 
   auto node = std::make_shared<Triangulator>();
