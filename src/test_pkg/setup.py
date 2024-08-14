9dd514ba27a21e63812456ac41976df67e4b6fd9
Joey <josephq02@outlook.com>
2024-08-14 15:50:58 +0000

    test fixes for formatting

--- src/test_pkg/setup.py
+++ src/test_pkg/setup.py
@@ -1,26 +1,23 @@
 from setuptools import find_packages, setup
 
-package_name = 'test_pkg'
+package_name = "test_pkg"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='autonomous',
-    maintainer_email='josephq02@outlook.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="autonomous",
+    maintainer_email="josephq02@outlook.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'tester = test_pkg.tester:main'
-        ],
+        "console_scripts": ["tester = test_pkg.tester:main"],
     },
 )
--- src/test_pkg/test_pkg/tester.py
+++ src/test_pkg/test_pkg/tester.py
@@ -1,11 +1,13 @@
 def main():
     a = 232323232323
     b = -23423423432423
-    c = ((a + b - a + b - a - b + a - b) * 43 / 1232 + a + b + a - b) * 69 / 420 + 12321321312
+    c = (
+        (a + b - a + b - a - b + a - b) * 43 / 1232 + a + b + a - b
+    ) * 69 / 420 + 12321321312
 
     myStr = "Hi I am a very long string to test formatting whaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaa aaaaaa"
-    print('Hi from test_pkg.')
+    print("Hi from test_pkg.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
