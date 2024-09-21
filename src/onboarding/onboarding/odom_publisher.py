9556255e1053b82336acd7407aba04f19cd79aec
jluc6628 <jayden6628@gmail.com>
2024-09-21 19:21:16 +0000

    wip_step2

---
+++ src/onboarding/onboarding/odom_publisher.py
@@ -0,0 +1,39 @@
+from typing import Any
+
+import rclpy
+from ackermann_msgs.msg import AckermannDriveStamped
+from rclpy.node import Node
+
+
+class OdomPublisher(Node):
+    def __init__(self) -> None:
+        super().__init__("odom_publisher")
+
+        self.declare_parameter("v", 0)
+        self.declare_parameter("d", 0)
+
+        self.publisher_ = self.create_publisher(AckermannDriveStamped, "drive", 10)
+
+        self.timer = self.create_timer(0.001, self.timer_callback)
+
+    def timer_callback(self) -> None:
+        v = self.get_parameter("v").value
+        d = self.get_parameter("d").value
+
+        msg = AckermannDriveStamped()
+        msg.drive.speed(v)
+        msg.drive.steering_angle(d)
+        msg.drive.stamp(self.get_clock().now())
+
+
+def main(args: Any = None) -> None:
+    rclpy.init(args=args)
+    odom_publisher = OdomPublisher()
+    rclpy.spin(odom_publisher)
+
+    odom_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "main":
+    main()
