2cb2d31d488a5fac5e7ca9b7244dacd2043425b1
jluc6628 <jayden6628@gmail.com>
2024-09-28 16:41:54 +0000

    wip_step_2

--- src/onboarding/setup.py
+++ src/onboarding/setup.py
@@ -1,25 +1,23 @@
 from setuptools import find_packages, setup
 
-package_name = 'onboarding'
+package_name = "onboarding"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='autonomous',
-    maintainer_email='jayden6628@gmail.com',
-    description='TODO: Package description',
-    license='Apache-2.0',
-    tests_require=['pytest'],
+    maintainer="autonomous",
+    maintainer_email="jayden6628@gmail.com",
+    description="TODO: Package description",
+    license="Apache-2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": ["odom_publisher = onboarding.odom_publisher:main"],
     },
 )
