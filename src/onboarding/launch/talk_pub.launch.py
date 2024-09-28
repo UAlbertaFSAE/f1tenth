c14dd289291980fd6d803cf38761fdb06a3b4847
fsoehn <fsoehn@ualberta.ca>
2024-09-28 11:24:00 -0600

    added docstring to launch file

--- src/onboarding/launch/talk_pub.launch.py
+++ src/onboarding/launch/talk_pub.launch.py
@@ -1,18 +1,26 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description() -> LaunchDescription:
-    return LaunchDescription([
-        Node(
-            package='onboarding',
-            namespace='odm_publisher',
-            executable='odm_publisher',
-            name='sim'
-        ),
-        Node(
-            package='onboarding',
-            namespace='odm_relay',
-            executable='odm_relay',
-            name='sim'
-        ),
-    ])
+    """Launchfile for odm relay and publisher.
+
+    Returns:
+        LaunchDescription: _description_
+    """
+    return LaunchDescription(
+        [
+            Node(
+                package="onboarding",
+                namespace="odm_publisher",
+                executable="odm_publisher",
+                name="sim",
+            ),
+            Node(
+                package="onboarding",
+                namespace="odm_relay",
+                executable="odm_relay",
+                name="sim",
+            ),
+        ]
+    )
