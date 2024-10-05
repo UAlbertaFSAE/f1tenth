280c5c7080dbebea7c676c72f43aa0062c8313a3
jluc6628 <jayden6628@gmail.com>
2024-10-05 18:04:02 +0000

    launch_file_wip

--- src/onboarding/launch/onboarding_launch.py
+++ src/onboarding/launch/onboarding_launch.py
@@ -1,2 +1,21 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+
+
+def generate_launch_description() -> LaunchDescription:
+    return LaunchDescription(
+        [
+            Node(
+                package="onboarding",
+                namespace="odom_publisher",
+                executable="odom_publisher",
+                name="publisher",
+            ),
+            Node(
+                package="onboarding",
+                namespace="odom_relay",
+                executable="odom_relay",
+                name="relay",
+            ),
+        ]
+    )
